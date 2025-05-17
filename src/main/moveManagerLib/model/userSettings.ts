1a918ffd7b7393512106ba48a2cf88e1475ba9cd
Vratislav Kalenda <v.kalenda@gmail.com>
2025-05-18 00:31:12 +0200

    fix: make update settings less strict

--- src/main/moveManagerLib/model/userSettings.ts
+++ src/main/moveManagerLib/model/userSettings.ts
@@ -10,9 +10,9 @@ export type UserSettings = {
 }
 
 export const UserSettingsZod = z.object({
-  sshPrivateKeyPath: z.string(),
-  sshKeyHasPassphrase: z.boolean(),
-  onboardingCompleted: z.boolean(),
+  sshPrivateKeyPath: z.string().optional(),
+  sshKeyHasPassphrase: z.boolean().optional(),
+  onboardingCompleted: z.boolean().optional(),
   sshCustomHostname: z.string().optional(),
   sshCustomPort: z.number().optional(),
   sshCustomUsername: z.string().optional()
